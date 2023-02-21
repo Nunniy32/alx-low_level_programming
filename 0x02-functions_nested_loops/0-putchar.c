4a4e6c470f9ae9419baa35d5f6f62defc770d891
Nunniy32 <light2dn@gmail.com>
2023-02-21 10:47:10 -0800

    ./0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
-/**
- * main - Entry point
- * Return: Always 0 (Success)
+/*
+ * main - Entery point
+ * Return: Always 0
  */
 int main(void)
-{
+{	
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -15,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);			
+	return (0);
 }
