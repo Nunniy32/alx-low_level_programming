3219281196d74216e55019fb96ebeb81bcff80ad
Nunniy32 <light2dn@gmail.com>
2023-02-21 12:36:33 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,13 +9,13 @@ int print_last_digit(int ld)
 {
 if (ld > 0)
 {
-	ld = ld % 10;
-	_putchar(ld + '0');
+ld = ld % 10;
+_putchar(ld + '0');
 }
 else
 {
-	ld = ld % 10 * -1;
-	_putchar(ld + '0');
+ld = ld % 10 * -1;
+_putchar(ld + '0');
 }
 return (ld);
 }
